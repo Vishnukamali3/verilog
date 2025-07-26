2c543e4dfd4fd11ec1176ba614cede5dc1b21285
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2025-07-26 20:45:27 +0530

    Add files via upload

--- mux4to1.v
+++ mux4to1.v
@@ -1,21 +1,16 @@
-module mux2to1(a,b,sel,y);
-  input a,b,sel;
+module mux4to1(a,sel,y);
+  input [3:0]a;
+  input [1:0]sel;
   output reg y;
   always@(*)
     begin
       case(sel)
-        1'b0:y=a;
-        1'b1:y=b;
-        default:y=1'bx;
+        2'b00 :y=a[0];
+        2'b01 :y=a[1];
+        2'b10 :y=a[2];
+        2'b11 :y=a[3];
+        default:y=2'bx;
       endcase
     end
 endmodule
-module mux4to1(a,sel,y);
-  input [3:0]a;
-  input [1:0]sel;
-  output y;
-  wire w1,w2;
-  mux2to1 u1(a[0],a[1],sel[0],w1);
-  mux2to1 u2(a[2],a[3],sel[0],w2);
-  mux2to1 u3(w1,w2,sel[1],y);
-endmodule
+  
